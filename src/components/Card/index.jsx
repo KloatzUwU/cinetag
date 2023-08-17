934580ea0b69022c85c6b792f4dab13307484886
KloatzUwU <kawan.borges.unico@gmail.com>
2023-08-17 15:14:12 -0300

    deploy na vercel

---
+++ src/components/Card/index.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import styles from './Card.module.css'
+import iconeFavoritar from './favoritar.png'
+import iconeDesfavoritar from './desfavoritar.png'
+import { useFavoritoContext } from 'components/Contextos/Favoritos'
+import { Link } from 'react-router-dom'
+
+export default function Card({id, titulo, capa}) {
+  const {favorito, adicionarFavorito} = useFavoritoContext()
+  const ehFavorito = favorito.some((fav) => fav.id === id)
+  const icone = ehFavorito ? iconeDesfavoritar : iconeFavoritar
+  return (
+    <div className={styles.container}>
+        <Link to={`/${id}`} className={styles.link}>
+          <img 
+                src={capa} 
+                alt={titulo} 
+                className={styles.capa} 
+            />
+            <h2>{titulo}</h2>
+        </Link>
+        <img 
+            src={icone} 
+            alt="Favoritar filme" 
+            className={styles.favoritar}    
+            onClick={() => {
+              adicionarFavorito({id, titulo, capa})
+            }}
+        />
+    </div>
+  )
+}
---
+++ src/pages/PaginaBase/index.jsx
@@ -0,0 +1,20 @@
+import Cabecalho from 'components/Cabecalho'
+import Container from 'components/Container'
+import FavoritosProvider from 'components/Contextos/Favoritos'
+import Rodape from 'components/Rodape'
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+
+export default function PaginaBase() {
+  return (
+    <main>
+        <Cabecalho />
+        <FavoritosProvider>
+            <Container>
+                <Outlet/>
+            </Container>
+        </FavoritosProvider>
+        <Rodape/>
+    </main>
+  )
+}
